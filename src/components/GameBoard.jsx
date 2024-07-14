b0fc787b8244abe72365e7efea3e43066889abdd
KeshavPandya01 <keshavpandya01@gmail.com>
2024-07-14 19:12:02 +0530

    gameboard edited

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,16 +2,6 @@
 
 
 const GameBoard = ({onSelectSquare, board}) => {
-  // const[GameBoard,setGameBoard] = useState(initialgameBoard)
-
-  // const handleSelectSquare = (rowIndex,colIndex) =>{
-  //   setGameBoard((prevGameBoard)=>{
-  //     const updatedBoard = [...prevGameBoard.map(innerArray =>[...innerArray])];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-  //   onSelectSquare();
-  // }
 
   return (
     <ol id="game-board">
